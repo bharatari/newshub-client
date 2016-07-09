efd819caced629fd0187ba0bbc771f6e6fdc0119
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-09 07:59:05 -0500

    Minor edits to FormatDate component

--- src/components/FormatDate.js
+++ src/components/FormatDate.js
@@ -3,14 +3,14 @@ import moment from 'moment';
 
 export default class FormatDate extends React.Component {
   static propTypes = {
-    date: PropTypes.object.isRequired
-  }
+    date: PropTypes.object.isRequired,
+  };
   formatDate = () => {
     return moment(this.props.date).format("MM/DD/YY");
-  }
+  };
   render() {
     return (
       <span>{this.formatDate()}</span>
-    )
+    );
   }
 };
