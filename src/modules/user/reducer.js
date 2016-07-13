6777ed8010558ddc7a8ee2b1ab798a675317c394
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-13 15:29:20 -0500

    Update user reducer

--- src/modules/user/reducer.js
+++ src/modules/user/reducer.js
@@ -1,6 +1,11 @@
 import { handleActions } from 'redux-actions';
 
 const initialState = {
+  fetchUser: {
+    requesting: false,
+    user: null,
+    error: null,
+  },
   createUser: {
     requesting: false,
     user: null,
@@ -9,6 +14,39 @@ const initialState = {
 };
 
 export default handleActions({
+  REQUEST_USER: (state, action) => ({
+    ...state,
+    fetchUser: {
+      ...state.fetchUser,
+      requesting: true,
+      user: null,
+      error: null,
+    },
+  }),
+  RECEIVE_USER: {
+    next(state, action) {
+      return {
+        ...state,
+        fetchUser: {
+          ...state.fetchUser,
+          requesting: false,
+          user: action.payload,
+          error: null,
+        },
+      };
+    },
+    throw(state, action) {
+      return {
+        ...state,
+        fetchUser: {
+          ...state.fetchUser,
+          requesting: false,
+          user: null,
+          error: action.payload,
+        },
+      };
+    }
+  },
   REQUEST_CREATE_USER: (state, action) => ({
     ...state,
     createUser: {
