8851416f6f737f469b3ac224a7a6db22cccf3bc6
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-14 10:38:45 -0500

    Fix issue with updateById not maintaining order of collection

--- src/utils/array.js
+++ src/utils/array.js
@@ -18,13 +18,17 @@ export default {
   updateById(collection, id, object) {
     // Deleting and then re-adding object so that the array
     // causes a rerender in React.
+    const index = _.findIndex(collection, (item) => {
+      return item.id === id;
+    });
     collection = this.deleteFromArrayById(collection, id);
 
     const updatedCollection = [
       ...collection,
-      object,
     ];
-
+    // Use splice with index to maintain order of the collection
+    updatedCollection.splice(index, 0, object);
+    
     return updatedCollection;
   },
   getDifferenceById(all, selected) {
