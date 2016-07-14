261c0a57a01d022ab24c15b820d3363ea7931883
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-13 19:53:09 -0500

    Move reservation status utilities to reservation utilities

--- src/modules/reservation/utils.js
+++ src/modules/reservation/utils.js
@@ -19,5 +19,45 @@ module.exports = {
     }
 
     return errors;
+  },
+  computeReservationStatus(reservation) {
+    if (!reservation.approved) {
+      return 'NEEDS_APPROVAL';
+    } else if (reservation.approved && !reservation.checkedOut && !reservation.checkedIn) {
+      return 'APPROVED';
+    } else if (reservation.approved && reservation.checkedOut && !reservation.checkedIn) {
+      return 'CHECKED_OUT';
+    } else if (reservation.approved && reservation.checkedOut && reservation.checkedIn) {
+      return 'CHECKED_IN';
+    } else {
+      return 'UNKNOWN';
+    }
+  },
+  getReservationStatus(reservation) {
+    return this.reservationStatus[this.computeReservationStatus(reservation)];
+  },
+  reservationStatus: {
+    'NEEDS_APPROVAL': 'Needs Approval',
+    'APPROVED': 'Approved',
+    'CHECKED_OUT': 'Checked Out',
+    'CHECKED_IN': 'Checked In',
+    'UNKNOWN': 'Unknown',
+  },
+  constructAdminAction(status) {
+    if (status === 'NEEDS_APPROVAL') {
+      return {
+        approved: true,
+      };
+    } else if (status === 'APPROVED') {
+      return {
+        checkedOut: true,
+      };
+    } else if (status === 'CHECKED_OUT') {
+      return {
+        checkedIn: true,
+      };
+    }
+
+    return null;
   }
 };
