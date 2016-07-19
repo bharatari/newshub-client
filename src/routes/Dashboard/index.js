4da188ef7c60febb91db01c44388ffbbd0ff139f
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-19 14:19:47 -0500

    Don't make dashboard a separate chuck, just inject reducer asynchronously

--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -1,20 +1,11 @@
 import { injectReducer } from '../../modules/';
+import Route from './Route';
+import reducer from './modules/reducer';
 
 export default (store) => ((nextState, cb) => {
-  /*  Webpack - use 'require.ensure' to create a split point
-      and embed an async module loader (jsonp) when bundling   */
-  require.ensure([], (require) => {
-    /*  Webpack - use require callback to define
-        dependencies for bundling   */
-    const Route = require('./Route').default
-    const reducer = require('./modules/reducer').default
+  /*  Add the reducer to the store on key 'counter'  */
+  injectReducer(store, { key: 'dashboard', reducer });
 
-    /*  Add the reducer to the store on key 'counter'  */
-    injectReducer(store, { key: 'dashboard', reducer })
-
-    /*  Return getComponent   */
-    cb(null, Route)
-
-  /* Webpack named bundle   */
-  }, 'dashboard');
+  /*  Return getComponent   */
+  cb(null, Route);
 });
