25ff206adaab26442835edfbd6139253edff4240
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-18 23:09:48 -0500

    Update not found (404) page

--- src/routes/NotFound/View.js
+++ src/routes/NotFound/View.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import classNames from 'classnames';
 import classes from './Styles.scss';
+
+const icon = classNames(
+  'ion-map',
+  classes.icon
+);
 
 export default class NotFoundView extends React.Component {
   render() {
     return (
       <div className="ui container">
         <div className={classes.container}>
-          <h1 className={classes.text}>Looks like you're lost.</h1>
+          <i className={icon}></i>
+          <h1 className={classes.header}>Looks like you're lost.</h1>
+          <p className={classes.text}>We couldn't find that page for you.</p>
         </div>
       </div>
     );
