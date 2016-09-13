1c8b07861730b4b170fe4db825872b59908ba666
Bharat Arimilli <bbarimilli@outlook.com>
2016-09-13 11:51:08 -0500

    Restructure routes and add Master container

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,7 @@ import Device from './Devices/routes/Device/Route';
 import authenticated from 'containers/Authenticated';
 import unauthenticated from 'containers/Unauthenticated';
 import admin from 'containers/Admin';
+import master from 'containers/Master';
 
 export default (store) => (
   <Route path="/" component={Root}>
@@ -27,12 +28,15 @@ export default (store) => (
       <Route path="/app/reservation/new" component={NewReservation} />
       <Route path="/app/reservation/:id" component={Reservation} />
       <Route path="/app/user" component={Users} />
-      <Route path="/app/user/new" component={NewUser} />
       <Route path="/app/user/:id" component={User} />
       <Route path="/app/device" component={Devices} />
-      <Route path="/app/device/new" component={NewDevice} />
       <Route path="/app/device/:id" component={Device} />
-      <Route component={admin()}></Route>
+      <Route component={admin()}>
+        <Route path="/app/user/new" component={NewUser} />
+      </Route>
+      <Route component={master()}>
+        <Route path="/app/device/new" component={NewDevice} />
+      </Route>
     </Route>
     <Route component={unauthenticated(Core)}>
       <Route path="/app/signup" component={Signup} />
