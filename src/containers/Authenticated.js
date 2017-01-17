3fe41860a58fd4adc5374b1d33697b94f4670ebd
Bharat Arimilli <bbarimilli@outlook.com>
2017-01-17 15:48:45 -0600

    Fix stale login bug

--- src/containers/Authenticated.js
+++ src/containers/Authenticated.js
@@ -5,6 +5,7 @@ import * as authentication from 'modules/authentication/actions';
 import * as user from 'modules/user/actions';
 import { loginRedirect } from 'constants/keys';
 import { routerActions } from 'react-router-redux';
+import _ from 'lodash';
 
 export default function authenticated(Component) {
   class AuthenticatedComponent extends React.Component {
@@ -24,7 +25,8 @@ export default function authenticated(Component) {
       this.props.actions.fetchCurrentUser();
     }
     componentWillReceiveProps(nextProps) {
-      if (!nextProps.requestingUser && !nextProps.user) {
+      console.log(nextProps.user);
+      if (!nextProps.requestingUser && (!nextProps.user || _.isError(nextProps.user))) {
         if (nextProps.currentRoute !== loginRedirect) {
           if (!this.state.requestedLogin) {
             this.props.actions.replace(loginRedirect + '?next=' + nextProps.currentRoute);
