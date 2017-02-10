cad71e55ae5e1e815a2b2c975e522aaff764fb17
Bharat Arimilli <bbarimilli@outlook.com>
2017-02-10 12:51:02 -0600

    Fix broken disable user checkbox on user page

--- src/routes/Users/routes/User/components/Master/Form.js
+++ src/routes/Users/routes/User/components/Master/Form.js
@@ -9,13 +9,9 @@ const button = classNames(
   classes.marginTop
 );
 
-const renderField = ({ input, meta: { touched, error }}) => (
-  <div className="field">
-    <div className="ui checkbox">
-      <input className={classes.font} type="checkbox" tabIndex="0" className="hidden" {...input} />
-      <label>Disable User</label>
-    </div>
-  </div>
+const checkbox = classNames(
+  'hidden',
+  classes.font
 );
 
 class UserForm extends React.Component {
@@ -37,7 +33,18 @@ class UserForm extends React.Component {
         <p className={classes.header}>Roles</p>
         <Field name="roles" component="input" type="text" className={classes.font} />
         <p className={classes.header}>Disabled</p>
-        <Field name="disabled" component={renderField} type="checkbox" className={classes.font} />
+        <div className="field">
+          <div className="ui checkbox">
+            <Field name="disabled" component="input" type="checkbox" className={classes.font} />
+            <label>Disable User</label>
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui checkbox">
+            <Field name="options.oNotDisturb" component="input" type="checkbox" className={classes.font} />
+            <label>Do Not Disturb</label>
+          </div>
+        </div>
         <button className={button} type="submit">UPDATE</button>
       </form>
     );
