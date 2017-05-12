00307f665e577a494a8b2260b3319befdabd3597
Bharat Arimilli <bbarimilli@outlook.com>
2017-05-12 15:55:03 -0500

    Edit NewReservationView test

--- src/routes/Reservations/routes/NewReservation/__tests__/View.spec.js
+++ src/routes/Reservations/routes/NewReservation/__tests__/View.spec.js
@@ -2,64 +2,23 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import View from 'routes/Reservations/routes/NewReservation/View';
 import { shallow } from 'enzyme';
-//import sinon from 'sinon';
+import sinon from 'sinon';
+import { expect } from 'chai';
 
-/*
 describe('(View) Dashboard', () => {
   let _props, _spies, _wrapper;
 
   beforeEach(() => {
     _spies = {};
+    _props = {
+      actions: {},
+      localActions: {},
+    };
   });
 
   it('should render as a <div>', () => {
-    _props = {};
     _wrapper = shallow(<View {..._props} />);
 
     expect(_wrapper.is('div')).to.equal(true);
   });
-
-  it('should have two save buttons', () => {
-    expect(_wrapper.find('button.save-button').to.have.length(2));
-  });
-
-  it('should call createReservation when header button is clicked', () => {
-    const actions = {
-      createReservation: sinon.spy(),
-    };
-
-    _props = {
-      actions,
-    };
-
-    _wrapper = shallow(<View {..._props} />);
-
-    _wrapper.find('button#new-reservation-header').first().simulate('click');
-
-    expect(actions.createReservation.calledOnce).to.be(true);
-  });
-
-  it('should call createReservation when form button is clicked', () => {
-    const actions = {
-      createReservation: sinon.spy(),
-    };
-
-    _props = {
-      actions,
-    };
-
-    _wrapper = shallow(<View {..._props} />);
-
-    _wrapper.find('button#new-reservation-form').first().simulate('click');
-
-    expect(actions.createReservation.calledOnce).to.be(true);
-  });
-
-  it('should show a response after success', () => {
-
-  });
-
-  it('should show a response after error', () => {
-
-  });
-});*/
+});
