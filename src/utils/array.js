4faacfb5d6790438901acceca731b112384b41c6
Bharat Arimilli <bbarimilli@outlook.com>
2017-05-15 12:24:57 -0500

    Add array utils tests

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,20 +1,6 @@
 import _ from 'lodash';
 
 export default {
-  getFromArray(collection, compare) {
-    let object = _.find(collection, function (item) {
-      return _.isEqual(item, compare);
-    });
-
-    return object;
-  },
-  getFromArrayById(collection, id) {
-    let object = _.find(collection, function (item) {
-      return item.id === id;
-    });
-
-    return object;
-  },
   updateById(collection, id, object) {
     // Deleting and then re-adding object so that the array
     // causes a rerender in React.
@@ -31,32 +17,6 @@ export default {
     
     return updatedCollection;
   },
-  getDifferenceById(all, selected) {
-    if (!_.isNil(all)) {
-      if (!_.isNil(selected)) {
-        let array = _.difference(_.pluck(all, 'id'), _.pluck(selected, 'id'));
-
-        for (let i = 0; i < array.length; i++) {
-          array[i] = this.getFromArrayById(all, array[i]);
-        }
-
-        return array;
-      } else {
-        return all;
-      }
-    } else {
-      return [];
-    }
-  },
-  deleteFromArray(array, item) {
-    const index = _.findIndex(array, (arrayItem) => {
-      return _.equals(item, arrayItem);
-    });
-
-    array.splice(index, 1);
-
-    return array;
-  },
   deleteFromArrayById(array, id) {
     const index = _.findIndex(array, (item) => {
       return item.id === id;
@@ -65,17 +25,6 @@ export default {
     array.splice(index, 1);
 
     return array;
-  },
-  existsById(array, id) {
-    const index = _.findIndex(array, (item) => {
-      return item.id === id;
-    });
-
-    if (!_.isNil) {
-      return true;
-    } else {
-      return false;
-    }
   },
   isObjectArray(array) {
     if (array) {
