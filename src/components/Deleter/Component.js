821b8f5e6391b58d9b741b41a529e3d51f1a2c5c
Bharat Arimilli <bbarimilli@outlook.com>
2018-02-24 16:07:04 -0600

    Migrate Deleter to Ant modal confirm

--- src/components/Deleter/Component.js
+++ src/components/Deleter/Component.js
@@ -1,46 +1,32 @@
 import React, { PropTypes } from 'react';
 import classes from './Styles.scss';
 import classNames from 'classnames';
-import { Modal } from 'components/';
 import access from 'utils/access';
 import _ from 'lodash';
+import { Button, Modal } from 'antd';
 
 export default class Deleter extends React.Component {
-  state = {
-    showModal: false,
-  };
   componentWillReceiveProps(nextProps) {
     // if requesting then deleted
     // navigate away
   }
-  deleteObject = () => {
-    this.props.delete(this.props.id);
-  };
+  /*
   handleClick = () => {
-    this.setState({
-      showModal: true,
-    });
-  };
+    Modal.confirm({
+      title: 'Are you sure you want to delete this?',
+      content: 'This action is irreversible.',
+      onOk() {
+        this.props.delete(this.props.id);
+      },
+    })
+  };*/
   render() {
     const { roles } = this.props;
     const canDelete = access.has(roles, `${this.props.model}:delete`);
 
     return (   
       <div>
-        { canDelete ? (
-            <button className="ui animated button red button-light" onClick={this.handleClick}>
-              <div className="visible content">DELETE</div>
-              <div className="hidden content">
-                <i className="trash icon"></i>
-              </div>
-            </button>
-          ) : null
-        }
-
-        <Modal id="deleter-modal" show={this.state.showModal} handleConfirmation={this.deleteObject} header="Are you sure?"
-          cancelText="Cancel" confirmationText="Delete">
-          Are you sure you want to delete this? This action is not reversible.
-        </Modal>
+        { canDelete ? <Button type="danger" onClick={this.handleClick}>Delete</Button> : null }
       </div>
     );
   }
