8673e24ca11a16551e6e0abe4f201cf515624ea1
Bharat Arimilli <bbarimilli@outlook.com>
2018-06-30 08:41:32 -0500

    Fix sidebar and menu bar

--- src/components/SidebarPage/Component.js
+++ src/components/SidebarPage/Component.js
@@ -3,7 +3,14 @@ import classes from './Styles.scss';
 import Sidebar from './Sidebar';
 import classNames from 'classnames';
 import { Notifications } from 'components/';
-import { Layout, Header, Content, Footer, Icon, Menu, Badge, Dropdown, Avatar } from 'antd';
+import { Layout, Header, Content, Footer, Icon, Menu, Badge, Dropdown, Avatar, notification } from 'antd';
+import { routerActions } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as user from 'modules/user/actions';
+import * as authentication from 'modules/authentication/actions';
+
+const SubMenu = Menu.SubMenu;
 
 const main = classNames(
   classes.main
@@ -18,7 +25,7 @@ const button = classNames(
   classes.button
 );
 
-export default class SidebarPage extends React.Component {
+class SidebarPage extends React.Component {
   static propTypes = {
     currentUrl: PropTypes.string,
     actions: PropTypes.object,
@@ -26,6 +33,7 @@ export default class SidebarPage extends React.Component {
     roles: PropTypes.array.isRequired,
   };
   state = {
+    switchedOrganization: false,
     active: false,
     collapsed: true,
   };
@@ -36,18 +44,72 @@ export default class SidebarPage extends React.Component {
       this.setState({ active: true });
     }
   }
+  handleMenu = (item, key, keyPath) => {
+    if (item.key === 'logout') {
+      this.props.actions.logout();
+    } else if (item.key.substring(0, 9) === '/app/user') {
+      this.props.actions.push(item.key);
+    } else {
+      this.props.actions.switchOrganization(this.props.user.id, item.key);
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if ((this.props.requestingSwitchOrganization && !nextProps.requestingSwitchOrganization) && nextProps.switchOrganization && !this.state.switchedOrganization) {
+      this.setState({
+        switchedOrganization: true,
+      });
+      
+      this.props.actions.push('/');
+
+      notification.success({
+        message: `Switched to ${nextProps.switchOrganization.currentOrganization.label}`,
+        description: 'To switch organizations again, use the organization switcher in the menu bar.'
+      });
+    } else if ((this.props.requestingSwitchOrganization && !nextProps.requestingSwitchOrganization) && nextProps.switchOrganizationError) {
+      notification.error({
+        message: `Error`,
+        description: 'Could not switch organizations. Try again later.'
+      });
+    }
+
+    if (nextProps.requestingSwitchOrganization) {
+      this.setState({
+        switchedOrganization: false,
+      });
+    }
+  }
   render() {
+    const dropdown = () => {
+      const organization = this.props.user.currentOrganization;
+      const organizations = this.props.user.organizations;
+
+      const list = [];
+
+      for (let i = 0; i < organizations.length; i++) {
+        const item = (
+          <Menu.Item key={organizations[i].id}>
+            {organizations[i].label}
+          </Menu.Item>
+        );
+
+        list.push(item);
+      }
+
+      return list;
+    }
+
     const menu = (
-      <Menu>
-        <Menu.Item>
+      <Menu onClick={this.handleMenu}>
+        <Menu.Item key={`/app/user/${this.props.user.id}`}>
           <Icon type="solution" />
           <span className={classes.dropdownText}>Profile</span>
         </Menu.Item>
-        <Menu.Item>
-          <Icon type="reload" />
-          <span className={classes.dropdownText}>Switch Organizations</span>
-        </Menu.Item>
-        <Menu.Item>
+
+        <SubMenu title={<span><Icon type="reload" /><span className={classes.dropdownText}>Switch Organizations</span></span>}>
+          {dropdown()}
+        </SubMenu>
+
+        <Menu.Item key="logout">
           <Icon type="logout" />
           <span className={classes.dropdownText}>Logout</span>
         </Menu.Item>
@@ -87,3 +149,21 @@ export default class SidebarPage extends React.Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  requestingSwitchOrganization: state.user.switchOrganization.requesting,
+  switchOrganization: state.user.switchOrganization.user,
+  switchOrganizationError: state.user.switchOrganization.error,
+});
+
+const actionCreators = {
+  ...routerActions,
+  ...authentication,
+  ...user,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actionCreators, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarPage);
--- src/components/SidebarPage/Sidebar.js
+++ src/components/SidebarPage/Sidebar.js
@@ -1,29 +1,11 @@
 import React, { PropTypes } from 'react';
-import { routerActions } from 'react-router-redux';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import classNames from 'classnames';
 import classes from './Styles.scss';
 import { configuration } from 'constants/routes';
-import userUtils from 'modules/user/utils';
 import access from 'utils/access';
-import * as notifications from 'modules/notifications/actions';
-import * as user from 'modules/user/actions';
-import * as authentication from 'modules/authentication/actions';
 import { Menu, Icon, Layout } from 'antd';
 
-const sidebar = classNames(
-  'desktop-only',
-  classes.sidebar,
-  classes.sidebarLeft,
-);
-
-const list = classNames(
-  'ui list',
-  classes.list
-);
-
-class Sidebar extends React.Component {
+export default class Sidebar extends React.Component {
   static propTypes = {
     currentUrl: PropTypes.string,
     actions: PropTypes.object,
@@ -31,14 +13,8 @@ class Sidebar extends React.Component {
     roles: PropTypes.array.isRequired,
   };
   state = {
-    switchedOrganization: false,
     collapsed: true,
   };
-  componentDidMount() {
-    $('.ui.dropdown')
-      .dropdown()
-    ;
-  }
   currentRoute = (url) => {
     if (url === this.props.currentUrl) {
       return true;
@@ -52,131 +28,7 @@ class Sidebar extends React.Component {
   handleClick = (item, key, keyPath) => {
     this.props.actions.push(item.key);
   };
-  handleLogout = () => {
-    this.props.actions.logout();
-  };
-  handleUser = () => {
-    this.props.actions.push('/app/user/' + this.props.user.id);
-  };
-  handleOrganizationClick = (id) => {
-    this.props.actions.switchOrganization(this.props.user.id, id);
-  };
-  componentWillReceiveProps(nextProps) {
-    if ((this.props.requestingSwitchOrganization && !nextProps.requestingSwitchOrganization) && nextProps.switchOrganization && !this.state.switchedOrganization) {
-      this.setState({
-        switchedOrganization: true,
-      });
-      
-      this.props.actions.push('/');
-
-      this.props.actions.pushNotification({
-        title: `Switched to ${nextProps.switchOrganization.currentOrganization.label}`,
-        body: 'To switch organizations again, use the organization switcher in the sidebar.',
-      });
-    } else if ((this.props.requestingSwitchOrganization && !nextProps.requestingSwitchOrganization) && nextProps.switchOrganizationError) {
-      this.props.actions.pushNotification({
-        title: 'Error',
-        body: 'Could not switch organizations. Try again later.',
-      });
-    }
-
-    if (nextProps.requestingSwitchOrganization) {
-      this.setState({
-        switchedOrganization: false,
-      });
-    }
-  }
   render() {
-    const mobileSidebar = classNames(
-      'ui inverted vertical mobile-only newshub-sidebar',
-      classes.sidebarMobile,
-      { active: this.props.active }
-    );
-
-    const organizationSelectorText = classNames(
-      'text',
-      classes.organizationSelectText,
-      classes.link
-    );
-
-    const organizationDropdown = classNames(
-      'ui inline dropdown',
-      classes.organizationDropdown
-    );
-
-    const dropdown = () => {
-      const organization = this.props.user.currentOrganization;
-      const organizations = this.props.user.organizations;
-
-      const list = [];
-
-      for (let i = 0; i < organizations.length; i++) {
-        const item = (
-          <div key={i} className="item" onClick={() => this.handleOrganizationClick(organizations[i].id)}>
-            <img className="ui avatar image" src={organizations[i].logo} />
-            {organizations[i].label}
-          </div>
-        );
-
-        list.push(item);
-      }
-
-      return (
-        <div className={organizationDropdown}>
-          <div className={organizationSelectorText}>
-            {organization.label}
-          </div>
-          <div className="menu">
-            {list}
-          </div>
-        </div>
-      );
-    };
-
-    const getButtons = () => {
-      const link = classNames(
-        'item',
-        classes.link
-      );
-      const buttons = classNames(
-        classes.buttons,
-        'ui list'
-      );
-      const person = classNames(
-        'ion-person',
-        classes.icon
-      );
-      const locked = classNames(
-        'ion-locked',
-        classes.icon
-      );
-      const loop = classNames(
-        'ion-loop',
-        classes.icon
-      );
-      
-      let user;
-      if (this.props.user) {
-        user = <a href="#" key="user" className={link} onClick={this.handleUser}>
-          <i className={person}></i><span className={classes.linkText}>{this.props.user.firstName}</span>
-        </a>
-      } else {
-        user = <a href="#" key="user" className={link}>
-          <span className={classes.linkText}></span>
-        </a>
-      }
-
-      return (
-        <div className={buttons}>
-          {dropdown()}
-          {user}
-          <a href="#" key="locked" className={link} onClick={this.handleLogout}>
-            <i className={locked}></i><span className={classes.linkText}>Logout</span>
-          </a>
-        </div>
-      );
-    };
-
     const getRoutes = () => {
       let routes = [];
 
@@ -213,22 +65,3 @@ class Sidebar extends React.Component {
     )
   }
 };
-
-const mapStateToProps = (state) => ({
-  requestingSwitchOrganization: state.user.switchOrganization.requesting,
-  switchOrganization: state.user.switchOrganization.user,
-  switchOrganizationError: state.user.switchOrganization.error,
-});
-
-const actionCreators = {
-  ...routerActions,
-  ...authentication,
-  ...notifications,
-  ...user,
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(actionCreators, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
